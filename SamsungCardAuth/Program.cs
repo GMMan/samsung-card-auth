d4349dc372775b5f028878cb65fe1751a9e370c2
Yukai Li <yukaili.geek@gmail.com>
2020-07-29 08:10:06 -0600

    Add project files.

---
+++ SamsungCardAuth/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SamsungCardAuth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
